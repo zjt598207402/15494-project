4fc3ad5b961dd236e3a5d151a4ea7b07d40c3f69
Keith M Yeung <kmyeung@theoden.rel.ri.cmu.edu>
2017-04-26 16:37:45 -0400

    Project Layout Setup

---
+++ Calibrator.py
@@ -0,0 +1,9 @@
+from cozmo_fsm import *
+from cozmo.util import Pose
+
+class Calibrate(StateNode):
+	#IMPLEMENT
+
+class AddWalls(StateNode):
+	#IMPLEMENT
+
---
+++ Movement.py
@@ -0,0 +1,11 @@
+from cozmo_fsm import *
+from cozmo.util import Pose
+
+class DecideMove(StateNode):
+	#IMPLEMENT
+
+class ForwardCustom(DriveForward):
+	#IMPLEMENT
+
+class TurnCustom(DriveTurn):
+	#IMPLEMENT
