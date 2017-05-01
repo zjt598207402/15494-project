a80a7e628dae7bc84167d211538d88cec078d269
Keith M Yeung <kmyeung@galadriel.rel.ri.cmu.edu>
2017-05-01 18:22:07 -0400

    edits

--- CV_Edge.py
+++ CV_Edge.py
@@ -16,30 +16,43 @@ class CV_Edge(StateMachineProgram):
         dummy = numpy.array([[0]*320])
         cv2.imshow('features',dummy)
 
-        cv2.createTrackbar('threshold1','features',10,1000,lambda self: None)
-        cv2.createTrackbar('threshold2','features',10,1000,lambda self: None)
+        cv2.createTrackbar('threshContour','features',170,1000,lambda self: None)
+        cv2.createTrackbar('threshold1','features',1000,1000,lambda self: None)
+        cv2.createTrackbar('threshold2','features',1000,1000,lambda self: None)
+        cv2.createTrackbar('votes','features',25,1000,lambda self: None)
 
         super().start()
 
     def user_image(self,image,gray):
+        threshContour = cv2.getTrackbarPos('threshContour', 'features')
         threshold1 = cv2.getTrackbarPos('threshold1', 'features')
         threshold2 = cv2.getTrackbarPos('threshold2', 'features')
-        self.edges = cv2.Canny(gray, threshold1, threshold2, apertureSize = 3)
+
+        ret, thresholded = cv2.threshold(gray, threshContour, 255, 0)
+        self.edges = cv2.Canny(thresholded, \
+            threshold1, threshold2, apertureSize = 3)
 
     def user_annotate(self,image):
         if self.edges is None: return image
-        lines = cv2.HoughLines(self.edges,1,np.pi/180,25)
+        votes = cv2.getTrackbarPos('votes', 'features')
+        lines = cv2.HoughLinesP(self.edges,1,np.pi/180,votes)
 
-        if (lines != None):
-            for rho,theta in lines[0]:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*rho
-                y0 = b*rho
-                x1 = int(x0 + 1000*(-b))
-                y1 = int(y0 + 1000*(a))
-                x2 = int(x0 - 1000*(-b))
-                y2 = int(y0 - 1000*(a))
+        # print(self.edges[0])
+        # return self.edges
 
-                cv2.line(image,(x1,y1),(x2,y2),(0,0,255),2)
+        if (lines is not None):
+            # for rho,theta in lines[0]:
+            #     a = np.cos(theta)
+            #     b = np.sin(theta)
+            #     x0 = a*rho
+            #     y0 = b*rho
+            #     x1 = int(x0 + 1000*(-b))
+            #     y1 = int(y0 + 1000*(a))
+            #     x2 = int(x0 - 1000*(-b))
+            #     y2 = int(y0 - 1000*(a))
+
+            #     cv2.line(image,(x1,y1),(x2,y2),(0,0,255),2)
+            for line in lines:
+                line = 2 * line[0]
+                cv2.line(image, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 2)
         return image
--- Movement.py
+++ Movement.py
@@ -1,8 +1,26 @@
 from cozmo_fsm import *
 from cozmo.util import Pose
+import asyncio, time
+
+async def build_walls():
+	if self.parent.lines = (1,1,1)
+		
+	if self.parent.lines = (1,0,1)
+
 
 class DecideMove(StateNode):
-	#IMPLEMENT
+	def start(self):
+		if (1,0,1):
+			self.post_success()
+		if (1,1,0):
+			angle = -90
+			self.post_failure()
+		if (0,1,1):
+			angle = 90
+			self.post_failure()
+		if (1,1,1):
+			angle = 180
+			self.post_failure()
 
 class ForwardCustom(DriveForward):
 	#IMPLEMENT
