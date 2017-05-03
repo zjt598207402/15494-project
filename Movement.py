106ccd1dee9bf2ab38410f7db9130284b53875ea
Justin Zhao <zhaojiateng@gmail.com>
2017-05-03 05:27:23 -0400

    Add files via upload

--- Movement.py
+++ Movement.py
@@ -2,11 +2,27 @@ from cozmo_fsm import *
 from cozmo.util import Pose
 import asyncio, time
 
-async def build_walls():
-	if self.parent.lines = (1,1,1)
-		
-	if self.parent.lines = (1,0,1)
+#needs a global list to hold all the walls
+wall_list[];
 
+async def build_walls():
+	#assume self.parent.lines is a tuple of binary (a,b,c)
+	d_to_next_box = 4.75
+	d_to_front = 2.375
+	d_to_left = 2.375
+	d_to_right = 2.375
+
+	#wall at left
+	if self.parent.lines[0] == 1:
+		#build wall at x = cozmo_x + d_to_next_box + d_to_front y = cozmo_y
+		
+	#wall at front
+	if self.parent.lines[1] == 1:
+		#build wall at x = cozmo_x + d_to_next_box  y = cozmo_y - d_to_left
+
+	#wall at right
+	if self.parent.lines[2] == 1:
+		#build wall at x = cozmo_x + d_to_next_box  y = cozmo_y + d_to_right
 
 class DecideMove(StateNode):
 	def start(self):
