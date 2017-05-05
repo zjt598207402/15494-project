77b64f15bc7ef6a6e4e9510dbf208fda73334d98
Justin Zhao <zhaojiateng@gmail.com>
2017-05-04 21:22:35 -0400

    done with movment

--- Movement.py
+++ Movement.py
@@ -3,43 +3,168 @@ from cozmo.util import Pose
 import asyncio, time
 
 #needs a global list to hold all the walls
-wall_list[];
+self.parent.walls = {}
 
-async def build_walls():
-	#assume self.parent.lines is a tuple of binary (a,b,c)
-	d_to_next_box = 4.75
-	d_to_front = 2.375
-	d_to_left = 2.375
-	d_to_right = 2.375
+class Build_walls(StateNode):
+	def start(self):
+		#assume self.parent.lines is a tuple of binary (a,b,c)
+	    #assume self.parent.cozmo_x is a tuple of coordinates (x,y)
+	    #assume self.parent.cozmo_head 0, 1, 2, 3 left front right bottom
+	    #assume value of each key [left,top.right,bottom]
 
-	#wall at left
-	if self.parent.lines[0] == 1:
-		#build wall at x = cozmo_x + d_to_next_box + d_to_front y = cozmo_y
-		
-	#wall at front
-	if self.parent.lines[1] == 1:
-		#build wall at x = cozmo_x + d_to_next_box  y = cozmo_y - d_to_left
 
-	#wall at right
-	if self.parent.lines[2] == 1:
-		#build wall at x = cozmo_x + d_to_next_box  y = cozmo_y + d_to_right
+	    #heading left
+	    if (self.parent.cozmo_head == 0):
+	    	if (self.parent.cozmo_x-1,self.parent.cozmo_y) not in self.parent.walls:
+	    		self.parent.walls[(self.parent.cozmo_x-1,self.parent.cozmo_y)]=[0,0,0,0]
 
+			#wall at left
+			if self.parent.curWall[0] == 1:
+				self.parent.walls[(self.parent.cozmo_x-1, self.parent.cozmo_y)][3] = 1
+
+			#wall at top
+			if self.parent.curWall[1] == 1:
+				self.parent.walls[(self.parent.cozmo_x-1, self.parent.cozmo_y)][0] = 1
+
+			#wall at right
+			if self.parent.curWall[2] == 1:
+				self.parent.walls[(self.parent.cozmo_x-1, self.parent.cozmo_y)][1] = 1
+
+
+		#heading top
+		if self.parent.cozmo_head == 1:
+			if (self.parent.cozmo_x,self.parent.cozmo_y+1) not in self.parent.walls:
+	    		self.parent.walls[(self.parent.cozmo_x,self.parent.cozmo_y+1)]=[0,0,0,0]
+
+			#wall at left
+			if self.parent.curWall[0] == 1:
+				self.parent.walls[(self.parent.cozmo_x, self.parent.cozmo_y+1)][0] = 1
+
+			#wall at top
+			if self.parent.curWall[1] == 1:
+				self.parent.walls[(self.parent.cozmo_x, self.parent.cozmo_y+1)][1] = 1
+
+			#wall at right
+			if self.parent.curWall[2] == 1:
+				self.parent.walls[(self.parent.cozmo_x, self.parent.cozmo_y+1)][2] = 1
+
+		#heading right
+		if self.parent.cozmo_head == 2:
+			if (self.parent.cozmo_x+1,self.parent.cozmo_y) not in self.parent.walls:
+	    		self.parent.walls[(self.parent.cozmo_x+1,self.parent.cozmo_y)]=[0,0,0,0]
+
+			#wall at left
+			if self.parent.curWall[0] == 1:
+				self.parent.walls[(self.parent.cozmo_x+1, self.parent.cozmo_y)][1] = 1
+
+			#wall at top
+			if self.parent.curWall[1] == 1:
+				self.parent.walls[(self.parent.cozmo_x+1, self.parent.cozmo_y)][2] = 1
+
+			#wall at right
+			if self.parent.curWall[2] == 1:
+				self.parent.walls[(self.parent.cozmo_x+1, self.parent.cozmo_y)][3] = 1
+
+		#heading bottom
+		if self.parent.cozmo_head == 3:
+			if (self.parent.cozmo_x,self.parent.cozmo_y-1) not in self.parent.walls:
+	    		self.parent.walls[(self.parent.cozmo_x,self.parent.cozmo_y-1)]=[0,0,0,0]
+
+			#wall at left
+			if self.parent.curWall[0] == 1:
+				self.parent.walls[(self.parent.cozmo_x, self.parent.cozmo_y-1)][2] = 1
+
+			#wall at top
+			if self.parent.curWall[1] == 1:
+				self.parent.walls[(self.parent.cozmo_x, self.parent.cozmo_y-1)][3] = 1
+
+			#wall at right
+			if self.parent.curWall[2] == 1:
+				self.parent.walls[(self.parent.cozmo_x, self.parent.cozmo_y-1)][0] = 1
+
+#use left front right moving policy
 class DecideMove(StateNode):
 	def start(self):
-		if (1,0,1):
-			self.post_success()
-		if (1,1,0):
-			angle = -90
-			self.post_failure()
-		if (0,1,1):
-			angle = 90
-			self.post_failure()
-		if (1,1,1):
-			angle = 180
-			self.post_failure()
+		#heading left
+		if self.parent.cozmo_head == 0:
+			if self.parent.walls[self.cozmo_x,self.cozmo_y][3] == 0:
+				self.parent.angle = 90
+				self.post_failure()
 
-class ForwardCustom(DriveForward):
-	#IMPLEMENT
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][0] == 0:
+				self.parent.distance = 120
+				self.post_success()
 
-class TurnCustom(DriveTurn):
-	#IMPLEMENT
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][1] == 0:
+				self.parent.angle = -90
+				self.post_failure()
+
+			else
+				self.parent.angle = 180
+				self.post_failure()
+
+		if self.parent.cozmo_head == 1:
+			if self.parent.walls[self.cozmo_x,self.cozmo_y][0] == 0:
+				self.parent.angle = 90
+				self.post_failure()
+				
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][1] == 0:
+				self.parent.distance = 120
+				self.post_success()
+
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][2] == 0:
+				self.parent.angle = -90
+				self.post_failure()
+
+			else
+				self.parent.angle = 180
+				self.post_failure()
+
+		if self.parent.cozmo_head == 2:
+			if self.parent.walls[self.cozmo_x,self.cozmo_y][1] == 0:
+				self.parent.angle = 90
+				self.post_failure()
+				
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][2] == 0:
+				self.parent.distance = 120
+				self.post_success()
+
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][3] == 0:
+				self.parent.angle = -90
+				self.post_failure()
+
+			else
+				self.parent.angle = 180
+				self.post_failure()
+
+		if self.parent.cozmo_head == 3:
+			if self.parent.walls[self.cozmo_x,self.cozmo_y][2] == 0:
+				self.parent.angle = 90
+				self.post_failure()
+				
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][3] == 0:
+				self.parent.distance = 120
+				self.post_success()
+
+			else if self.parent.walls[self.cozmo.x,self.cozmo_y][0] == 0:
+				self.parent.angle = -90
+				self.post_failure()
+
+			else
+				self.parent.angle = 180
+				self.post_failure()
+
+
+
+class ForwardCustom(Forward):
+	def start(self, event = None):
+        if self.running: return
+        self.distance= distance_mm(self.parent.distance)
+        super().start()
+
+
+class TurnCustom(Turn):
+	def start(self, event = None):
+        if self.running: return
+        self.angle = degrees(self.parent.angle)
+        super().start()
